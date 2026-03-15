c6efc52d32d56f5e8f229fa48dc6bdc4edc62a55
eze <91032516+Ezequiel138@users.noreply.github.com>
2026-03-14 22:54:09 -0300

    Fix HttpClient include path for Geode builds

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,237 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/CustomSongLayer.hpp>
+#include <Geode/modify/EditLevelLayer.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/modify/LevelSettingsLayer.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
+#include <extensions/network/HttpClient.h>
+#include <fstream>
+#include <unordered_map>
+
 using namespace geode::prelude;
+using namespace cocos2d::extension;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+class SongURLDownloader : public CCNode {
+protected:
+    std::unordered_map<CCHttpRequest*, int> m_pendingSongIds;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+public:
+    static SongURLDownloader* get() {
+        static SongURLDownloader* instance = nullptr;
+        if (!instance) {
+            instance = new SongURLDownloader();
+            instance->init();
+            instance->retain();
+        }
+        return instance;
+    }
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+    void downloadSongFromURL(std::string const& url, int songID) {
+        auto request = new CCHttpRequest();
+        request->setUrl(url.c_str());
+        request->setRequestType(CCHttpRequest::kHttpGet);
+        request->setTag("more-songs-downloads");
+        request->setResponseCallback(this, httpresponse_selector(SongURLDownloader::onSongDownloaded));
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+        m_pendingSongIds.emplace(request, songID);
+        CCHttpClient::getInstance()->send(request);
+        request->release();
+    }
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+    void onSongDownloaded(CCHttpClient*, CCHttpResponse* response) {
+        if (!response) {
+            FLAlertLayer::create("Music URL", "Download failed (no response).", "OK")->show();
+            return;
+        }
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+        auto request = response->getHttpRequest();
+        if (!request || !m_pendingSongIds.contains(request)) {
+            FLAlertLayer::create("Music URL", "Unexpected download state.", "OK")->show();
+            return;
+        }
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+        auto songID = m_pendingSongIds.at(request);
+        m_pendingSongIds.erase(request);
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+        if (!response->isSucceed()) {
+            FLAlertLayer::create("Music URL", response->getErrorBuffer(), "OK")->show();
+            return;
+        }
+
+        auto path = MusicDownloadManager::sharedState()->pathForSong(songID);
+        auto data = response->getResponseData();
+
+        std::ofstream file(path, std::ios::binary);
+        if (!file.is_open()) {
+            FLAlertLayer::create("Music URL", "Couldn't write downloaded song file.", "OK")->show();
+            return;
+        }
+
+        file.write(data->data(), static_cast<std::streamsize>(data->size()));
+        file.close();
+
+        FLAlertLayer::create(
+            "Music URL",
+            fmt::format(
+                "Downloaded and installed successfully.\\nUse Song ID: <cg>{}</c>",
+                songID
+            ).c_str(),
+            "OK"
+        )->show();
+    }
+};
+
+class SongURLImportPopup : public Popup<> {
+protected:
+    TextInput* m_urlInput = nullptr;
+
+    bool setup() override {
+        this->setTitle("Insert music URL:");
+
+        m_urlInput = TextInput::create(250.f, "https://...");
+        m_urlInput->setPosition(m_size.width / 2.f, m_size.height / 2.f + 5.f);
+        m_mainLayer->addChild(m_urlInput);
+
+        auto menu = CCMenu::create();
+        menu->setPosition({ m_size.width / 2.f, 35.f });
+        m_mainLayer->addChild(menu);
+
+        auto confirmSpr = ButtonSprite::create("Confirm");
+        auto confirmBtn = CCMenuItemSpriteExtra::create(
+            confirmSpr,
+            this,
+            menu_selector(SongURLImportPopup::onConfirm)
+        );
+        menu->addChild(confirmBtn);
+
+        return true;
+    }
+
+    int songIDForURL(std::string const& url) {
+        auto hashValue = static_cast<uint32_t>(std::hash<std::string>{}(url));
+        return static_cast<int>(10000000 + (hashValue % 89999999));
+    }
+
+    void onConfirm(CCObject*) {
+        auto url = m_urlInput->getString();
+        if (url.empty()) {
+            FLAlertLayer::create("Music URL", "Please paste a URL first.", "OK")->show();
+            return;
+        }
+
+        if (!string::startsWith(url, "http://") && !string::startsWith(url, "https://")) {
+            FLAlertLayer::create("Music URL", "The URL must start with http:// or https://", "OK")->show();
+            return;
+        }
+
+        auto songID = songIDForURL(url);
+        SongURLDownloader::get()->downloadSongFromURL(url, songID);
+
+        FLAlertLayer::create(
+            "Music URL",
+            fmt::format("Downloading now...\\nGenerated Song ID: <cy>{}</c>", songID).c_str(),
+            "OK"
+        )->show();
+        this->onClose(nullptr);
+    }
+
+public:
+    static SongURLImportPopup* create() {
+        auto ret = new SongURLImportPopup();
+        if (ret && ret->initAnchored(300.f, 190.f)) {
+            ret->autorelease();
+            return ret;
+        }
+
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
+};
+
+
+class PopupOpenTarget : public CCNode {
+public:
+    static PopupOpenTarget* get() {
+        static PopupOpenTarget* inst = nullptr;
+        if (!inst) {
+            inst = new PopupOpenTarget();
+            inst->init();
+            inst->retain();
+        }
+        return inst;
+    }
+
+    void onOpen(CCObject*) {
+        if (auto popup = SongURLImportPopup::create()) {
+            popup->show();
+        }
+    }
+};
+
+static void addOpenURLButton(CCLayer* layer, CCPoint position, std::string const& id) {
+    auto menu = typeinfo_cast<CCMenu*>(layer->getChildByID("url-import-menu"));
+    if (!menu) {
+        menu = CCMenu::create();
+        menu->setID("url-import-menu");
+        menu->setPosition({0.f, 0.f});
+        layer->addChild(menu, 50);
+    }
+
+    if (menu->getChildByID(id)) {
+        return;
+    }
+
+    auto button = CCMenuItemSpriteExtra::create(
+        ButtonSprite::create("URL", "goldFont.fnt", "GJ_button_04.png", 0.8f),
+        PopupOpenTarget::get(),
+        menu_selector(PopupOpenTarget::onOpen)
+    );
+
+    button->setID(id);
+    button->setPosition(position);
+    menu->addChild(button);
+}
+
+class $modify(LevelSettingsLayerMusicURL, LevelSettingsLayer) {
+    bool init(LevelSettingsObject* settings, LevelEditorLayer* editor) {
+        if (!LevelSettingsLayer::init(settings, editor)) {
+            return false;
+        }
+
+        addOpenURLButton(this, { 40.f, 255.f }, "url-import-btn-editor"_spr);
+        return true;
+    }
+};
+
+class $modify(EditLevelLayerMusicURL, EditLevelLayer) {
+    bool init(GJGameLevel* level) {
+        if (!EditLevelLayer::init(level)) {
+            return false;
+        }
+
+        addOpenURLButton(this, { 40.f, 255.f }, "url-import-btn-outside-editor"_spr);
+        return true;
+    }
+};
+
+class $modify(LevelInfoLayerMusicURL, LevelInfoLayer) {
+    bool init(GJGameLevel* level, bool challenge) {
+        if (!LevelInfoLayer::init(level, challenge)) {
+            return false;
+        }
+
+        addOpenURLButton(this, { 40.f, 255.f }, "url-import-btn-level-info"_spr);
+        return true;
+    }
+};
+
+class $modify(CustomSongLayerMusicURL, CustomSongLayer) {
+    bool init(LevelSettingsObject* settings) {
+        if (!CustomSongLayer::init(settings)) {
+            return false;
+        }
+
+        addOpenURLButton(this, { 35.f, 250.f }, "url-import-btn-custom-song"_spr);
+        return true;
+    }
 };
